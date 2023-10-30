cacf4de4a000e88709246d00eb45d0c71f4c9df6
快乐的老鼠宝宝 <keaitianxinmail@qq.com>
2023-10-30 16:23:19 +0800

    添加规则，继续抽象不同的处理函数，保持主函数整洁
    规则来自
    https://t.me/the_ccsn/15088
    为该同学与开发者的私聊中复制

--- src/bibtex-pruner/__main__.py
+++ src/bibtex-pruner/__main__.py
@@ -1,28 +1,35 @@
-# remove all url/file/snapshot before latex build, every zotero extrace should run once.
-
+rule_1 = ["title", "author", "journaltitle", "volume", "number", "pages", "langid"]
+rule_2 = ["date", "eprint", "eprinttype", "issn", "doi", "keywords", "file"]
 link_black_list = ["kns.cnki.net", "wanfangdata.com.cn", "github.com"]
+
+
+def fliter_link(bib_line_list):
+    # remove all url/file/snapshot before latex build
+    new_bib = []
+    for line in bib_line_list:
+        flag_exist = True
+        for domain in link_black_list:
+            if domain in line:
+                flag_exist = False
+        if flag_exist == True:
+            new_bib.append(line)
+
+    return "\n".join(new_bib)
+
+
+def fliter_doi():
+    pass
+
 
 def main():
     bib_file = open("ref.bib", "r", encoding="utf-8")
     bib = bib_file.readlines()
-
-    def new_bib_func(bib):
-        new_bib = []
-        for line in bib:
-            flag_exist = True
-            for domain in link_black_list:
-                if domain in line:
-                    flag_exist = False
-            if flag_exist == True:
-                new_bib.append(line)
-        
-        return "\n".join(new_bib)
-    
     bib_file.close()
     bib_file = open("ref.bib", "w", encoding="utf-8")
-    bib_file.writelines(new_bib_func(bib))
+    bib_file.writelines(fliter_link(bib))
     bib_file.close()
 
 
 if __name__ == "__main__":
+    # TODO 添加支持自定义bib文件
     main()
